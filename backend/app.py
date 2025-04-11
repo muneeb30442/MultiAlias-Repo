bf604cdad0fc75f84d8ea5dc634a5cbe1a3d77bc
SyedAhmed-1 <zenkop46@gmail.com>
2025-04-11 17:39:51 +0500

    final v1

--- backend/app.py
+++ backend/app.py
@@ -23,7 +23,7 @@ with open(MODEL_PATH, 'rb') as f:
 def save_to_db(name, age, contactNo, mean_val, symptoms, prediction, image_filename):
     conn = sqlite3.connect(DB_PATH)
     c = conn.cursor()
-    c.execute('DROP TABLE IF EXISTS predictions') #use this only when to clean the database locally 
+    # c.execute('DROP TABLE IF EXISTS predictions') #use this only when to clean the database locally 
 
     # ✅ schema
     c.execute('''CREATE TABLE IF NOT EXISTS predictions (
